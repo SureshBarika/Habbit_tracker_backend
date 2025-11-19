013c308f3c170029d7e623cdb3394c7bb0edfb5c
Suresh Barika <sureshbarika9480@gmail.com>
2025-11-19 15:52:41 +0530

    fixed some errors

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://your-frontend.onrender.com",  // change this
+  "https://habbit-tracker-frontend-black.vercel.app",  // change this
 ];
 
 app.use(
